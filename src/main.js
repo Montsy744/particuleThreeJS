daadb853812089efb86d165403b34e6676b149d6
Hugo Straseele <straseelehugo@gmail.com>
2025-07-10 20:21:05 +0200

    TIMER => 36.19

--- src/main.js
+++ src/main.js
@@ -1,12 +1,22 @@
-import { PerspectiveCamera, Scene, WebGLRenderer, Mesh, BoxGeometry, MeshNormalMaterial } from 'three';
+import { PerspectiveCamera, Scene, WebGLRenderer, PointsMaterial, BufferGeometry, Float32BufferAttribute, MathUtils } from 'three';
 import { AxesHelper } from 'three';
+import { Group } from 'three';
+import { Points } from 'three';
+import { TextureLoader } from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import './style.css'
 
 
-const scene = new Scene();
+const textureLoader = new TextureLoader();
+const ciculeTexture = textureLoader.load('/circle.png');
+const alphaMap = textureLoader.load('/alphamap.png');
 
-scene.add(new AxesHelper())
+
+const scene = new Scene();
+const count = 100;
+const distance = 2;
+
+//scene.add(new AxesHelper())
 
 const camera = new PerspectiveCamera(
   75, 
@@ -19,15 +29,37 @@ camera.position.y = 0.5;
 camera.position.x = 0.5;
 scene.add(camera);
 
-const cub = new Mesh(
-  new BoxGeometry(1, 1, 1),
-  new MeshNormalMaterial()
-);
-scene.add(cub);
+const points = new Float32Array(count * 3);
+const colors = new Float32Array(count * 3);
+for (let i = 0; i< points.length; i++) {
+  points[i] = MathUtils.randFloatSpread(distance*2);
+
+  colors[i] = Math.random();
+}
+
+const geometry = new BufferGeometry();
+geometry.setAttribute('position', new Float32BufferAttribute(points, 3));
+
+geometry.setAttribute('color', new Float32BufferAttribute(colors, 3));
+const pointMaterial = new PointsMaterial({
+  size: 0.1,
+  vertexColors: true,
+  //map: ciculeTexture,
+  alphaTest: 0.5,
+  alphaMap: alphaMap,
+  transparent: true,
+});
+const point = new Points(geometry,pointMaterial);
+
+const group = new Group();
+group.add(point);
+scene.add(group);
 
 const renderer = new WebGLRenderer({
   antialias: true,
+  alpha: true,
 });
+renderer.setClearColor(0x000000, 0);
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2)); 
 document.body.appendChild(renderer.domElement)
